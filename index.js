75f2cec71fd364289f525bf6b9d305f090943a18
thq <liangyue.thq@raycloud.com>
2022-07-05 15:02:46 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,4 @@
+var res = {
+    a: '123'
+};
+console.log(res === null || res === void 0 ? void 0 : res.a);
---
+++ index.ts
@@ -0,0 +1,9 @@
+interface A {
+    a: string;
+}
+
+const res:A = {
+    a: '123'
+}
+
+console.log(res?.a);
